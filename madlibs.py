ec0ea5f3ec9dbc5f88cfd47a5a7e060da9fa82ac
MariaBeatriceNamatovu <namatovutrismari@gmail.com>
2024-01-27 21:35:44 +0300

    madlib

---
+++ madlibs.py
@@ -0,0 +1,22 @@
+#3 ways to concatenate strings
+#if we want to create a string that says: "subscribe to ___"
+#there are 3 ways to do this;
+# youtuber = "Kyile ying"
+# print("subscribe to" + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+
+adj = input("Adjective: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2:")
+famous_person = input("famous_person: ")
+
+madlib = f"Computer programming is so {adj}! it makes me so excited all the time because \
+I love to {verb1}. stay hydrates and {verb2} like you are {famous_person}!"
+
+print(madlib)
+
+
+
+
